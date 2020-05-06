7d49cb50527984f823f97db63c03243a2d69a2d2
Ewan <ewan.cook@live.com>
2020-05-07 00:17:16 +0100

    fix failing collator test - failed due to addition of multiprocessing.Process

--- transcriber/converter/test_collator.py
+++ transcriber/converter/test_collator.py
@@ -13,14 +13,34 @@ file_contents = """Date,Time,Tag
 
 
 class TestCollator(unittest.TestCase):
+    def _fake_process(self, target=None, args=[]):
+        collator._collate(args[0], args[1])
+        return None
+
+    @mock.patch("multiprocessing.Process.__init__", new=_fake_process)
+    @mock.patch("multiprocessing.Process.join")
+    @mock.patch("multiprocessing.Process.start")
     @mock.patch("transcriber.converter.collator.collate_files")
     @mock.patch("builtins.open", new_callable=mock.mock_open)
-    def test_collator_collate(self, mock_file, collate_files):
+    def test_collator_collate(
+        self, mock_file, collate_files, mock_start, mock_join
+    ):
         save_file = "this/is/the/collated/file.csv"
         filenames = ["this/is/a/test.csv"]
         collator.Collator().collate(save_file, filenames)
         collate_files.assert_called_with(
             mock_file(), [transcribed_filename(f) for f in filenames]
+        )
+        mock_start.assert_called_once()
+        mock_join.assert_called_once()
+
+    @mock.patch("transcriber.converter.collator.collate_files")
+    def test__collate(self, mock_collate):
+        save_file = "this/is/the/collated/file.csv"
+        filenames = ["this/is/a/test.csv"]
+        collator._collate(save_file, filenames)
+        mock_collate.assert_called_with(
+            save_file, [transcribed_filename(f) for f in filenames]
         )
 
     def test_collate_files(self):
